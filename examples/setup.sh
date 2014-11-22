0a595d72198f167527b7baece391611f1c877e6d
Makis Tracend <makis.tracend@gmail.com>
2014-11-22 13:16:41 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the Apptrack CLI
+
+# 1. Install the main executable
+npm install -g apptrack-cli
+
+#2. Setup Apptrack (only needed once)
+apptrack setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+apptrack -h
